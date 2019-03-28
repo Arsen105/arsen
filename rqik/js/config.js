1c8dc37ca7afaf36ddc7cf64d9ca527c976cfb6c
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2019-03-28 13:11:45 +0300

    ss

---
+++ rqik/js/config.js
@@ -0,0 +1,52 @@
+"use strict";
+var planet = "земля";
+var name = "Петя";
+// alert(name);
+      // var password;
+      // var userName = prompt("Введите логин", null);
+      // if (userName == "Admin") {
+      //    password = prompt("Введите пароль", '');
+      //    if (password == "Черный властелин") {
+      //       alert("Добро пожаловать!");
+      //    } else if (password == null) {
+      //       alert("пароль неверен ");
+      //    } else {
+      //       alert("вход отменен");
+      //    }
+
+      // } else if (userName == null) {
+      //    alert("вход отменен");
+      // } else {
+      //    alert("Я вас незнаю")
+      // }
+
+// var a=2, c =4;
+// var b=prompt("Введите B", "0");
+
+//   var resulte=(a+ +b<=c) ? "MIN" : "MAX";
+//   alert(b);
+//   alert(a+"" +c);
+
+// alert(resulte);
+
+
+var message;
+(login == 'Вася')?
+  message = 'Привет': 
+(login == 'Директор')? 
+  message = 'Здравствуйте' :  (login == '') ? 
+  message = 'Нет логина' : 
+  message = ' ' ;
+
+
+//alert(userName);
+//alert(123.432 ^0);
+//var numB = prompt('',123);
+// function returnFalse()
+// {  
+//    if (isIteger(numB)){
+//       alert(numB+'Целое число');
+//    }
+//    alert("не целое цисло");
+// }
+// returnFalse();
