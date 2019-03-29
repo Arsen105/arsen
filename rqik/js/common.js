37846daa144072e71f2e953d7bf3060b819782f0
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2019-03-29 21:09:57 +0300

    best1

--- rqik/js/common.js
+++ rqik/js/common.js
@@ -1,3 +1,16 @@
-$(function(){
-  // $('body').fadeout();
+$(document).ready(function() {
+
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "/js/mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
 });
